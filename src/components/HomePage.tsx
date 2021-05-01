92d3bfc005e1c46ef29de1c40e88ca392676e9f3
Libre255 <briangbv@outlook.com>
2021-05-01 22:31:16 +0200

    feature/7-configure-homePage-and-align-basic-template-for-game v1 finish

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import GameMainComponent from './gameplayComponents/GameMainComponent'
 
 const HomePage :React.FC = () => {
 
   return(
-    <div>
-       Hello world
+    <div id="homePage" className="testBox">
+       <GameMainComponent/>
     </div>
   )
 }
