02eeca24c11c17a1e7de54de8e5d490be54b90c9
Libre255 <briangbv@outlook.com>
2021-05-26 00:03:11 +0200

    feature/2-create-basic-menu-with-Home-And-Ranking v1 finish

--- app/src/components/gameplayComponents/GamePlay/Card.tsx
+++ app/src/components/gameplayComponents/GamePlay/Card.tsx
@@ -15,14 +15,15 @@ const Card: React.FC<Props> = ({ selectedCardsState, cardState }) => {
   const [cardValue, setCards] = cardState;
   const [selectedCards, setSelectedCards] = selectedCardsState;
 
+  const flipClickedCardFaceUP = (cardDeck: CardsAPI[]) =>
+    cardDeck.map((card) => {
+      if (card.id === cardValue.id) card.flippCard = true;
+      return card;
+    });
+
   const onCardClick = () => {
     if (selectedCards.length < 2) {
-      setCards((pv) =>
-        pv.map((card) => {
-          if (card.id === cardValue.id) card.flippCard = true;
-          return card;
-        })
-      );
+      setCards((cardDeck) => flipClickedCardFaceUP(cardDeck));
       setSelectedCards((pv) => [...pv, cardValue]);
     }
   };
