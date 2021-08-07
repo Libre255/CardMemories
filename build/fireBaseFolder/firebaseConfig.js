9e0af0457686d613fa5095712aba7be29426dc25
Libre255 <briangbv@outlook.com>
2021-08-08 01:37:51 +0200

    5

--- build/fireBaseFolder/firebaseConfig.js
+++ build/fireBaseFolder/firebaseConfig.js
@@ -11,7 +11,7 @@ firebase_admin_1.default.initializeApp({
         "type": process.env.TYPE,
         "project_id": process.env.FIREBASE_PROJECT_ID,
         "private_key_id":process.env.FIREBASE_KEYID,
-        "private_key": process.env.FIREBASE_PRIVATE_KEY,
+        "private_key": process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
         "client_email": process.env.FIREBASE_CLIENT_EMAIL,
         "client_id": process.env.CLIENT_ID,
         "auth_uri": process.env.AUTH_URI ,
