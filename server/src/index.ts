63c89ec79a0e19d18dc161f588c85bfaf3cbe4a1
Libre255 <briangbv@outlook.com>
2021-08-08 16:14:59 +0200

    asd

--- server/src/index.ts
+++ server/src/index.ts
@@ -9,7 +9,7 @@ const serverPORT = process.env.PORT || 8080;
 
 app.use(express.json());
 app.use(cors());
-app.use(express.static(path.join(__dirname + '/../../app/build')));
+app.use(express.static(path.join(__dirname + '/../build')));
 
 app.use('/cards', cardsAPI);
 app.use('/ranking', rankingAPI);
@@ -19,7 +19,7 @@ app.get('/', (req, res) => {
 })
 
 app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname + '/../../app/build'), function (err) {
+  res.sendFile(path.join(__dirname + '/../build/index.html'), function (err) {
       if (err) {
           res.status(500).send(err)
       }
