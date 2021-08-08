7a5031982ac604c65b1b4ce8fa565d8fc115336c
Libre255 <briangbv@outlook.com>
2021-08-08 11:11:51 +0200

    finally

--- build/fireBaseFolder/firebaseConfig.js
+++ build/fireBaseFolder/firebaseConfig.js
@@ -8,9 +8,16 @@ var firebase_admin_1 = __importDefault(require("firebase-admin"));
 require('dotenv').config();
 firebase_admin_1.default.initializeApp({
     credential: firebase_admin_1.default.credential.cert({
-        "projectId": process.env.FIREBASE_PROJECT_ID,
+        "project_id": process.env.FIREBASE_PROJECT_ID,
         "private_key": process.env.FIREBASE_PRIVATE_KEY,
         "client_email": process.env.FIREBASE_CLIENT_EMAIL,
+        "type": process.env.TYPE,
+        "private_key_id": process.env.FIREBASE_KEYID,
+        "auth_uri": process.env.AUTH_URI,
+        "auth_provider_x509_cert_url": process.env.AUTH_CERT,
+        "token_uri": process.env.TOKEN_URI,
+        "client_x509_cert_url": process.env.CLIENT_AUTH,
+        "client_id": process.env.CLIENT_ID
     })
 });
 var firebaseDB = firebase_admin_1.default.firestore();
--- server/src/fireBaseFolder/firebaseConfig.ts
+++ server/src/fireBaseFolder/firebaseConfig.ts
@@ -3,9 +3,16 @@ require('dotenv').config()
 
 admin.initializeApp({
   credential:admin.credential.cert({
-    "projectId": process.env.FIREBASE_PROJECT_ID,
+    "project_id": process.env.FIREBASE_PROJECT_ID,
     "private_key": process.env.FIREBASE_PRIVATE_KEY,
     "client_email": process.env.FIREBASE_CLIENT_EMAIL,
+    "type":process.env.TYPE,
+    "private_key_id":process.env.FIREBASE_KEYID,
+    "auth_uri":process.env.AUTH_URI,
+    "auth_provider_x509_cert_url":process.env.AUTH_CERT,
+    "token_uri":process.env.TOKEN_URI,
+    "client_x509_cert_url":process.env.CLIENT_AUTH,
+    "client_id":process.env.CLIENT_ID
   } as ServiceAccount)
 })
 
