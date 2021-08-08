d9a2e75b9cdc77a743e6b082cd71bd7e61c95437
Libre255 <briangbv@outlook.com>
2021-08-08 11:19:11 +0200

    now

--- server/src/fireBaseFolder/firebaseConfig.ts
+++ server/src/fireBaseFolder/firebaseConfig.ts
@@ -4,7 +4,7 @@ require('dotenv').config()
 admin.initializeApp({
   credential:admin.credential.cert({
     "project_id": process.env.FIREBASE_PROJECT_ID,
-    "private_key": process.env.FIREBASE_PRIVATE_KEY,
+    "private_key": process.env.FIREBASE_PRIVATE_KEY!.replace(/\\n/g, '\n'),
     "client_email": process.env.FIREBASE_CLIENT_EMAIL,
     "type":process.env.TYPE,
     "private_key_id":process.env.FIREBASE_KEYID,
