fed7f024331b9a0f48edbf6b2ebfff0368b426dc
Libre255 <briangbv@outlook.com>
2021-08-08 11:19:35 +0200

    123

--- build/fireBaseFolder/firebaseConfig.js
+++ build/fireBaseFolder/firebaseConfig.js
@@ -9,7 +9,7 @@ require('dotenv').config();
 firebase_admin_1.default.initializeApp({
     credential: firebase_admin_1.default.credential.cert({
         "project_id": process.env.FIREBASE_PROJECT_ID,
-        "private_key": process.env.FIREBASE_PRIVATE_KEY,
+        "private_key": process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
         "client_email": process.env.FIREBASE_CLIENT_EMAIL,
         "type": process.env.TYPE,
         "private_key_id": process.env.FIREBASE_KEYID,
