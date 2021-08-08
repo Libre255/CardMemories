bbda85640a790a47309940238ed45d65b6b7dbc8
Libre255 <briangbv@outlook.com>
2021-08-08 13:50:38 +0200

    2313

--- server/src/index.ts
+++ server/src/index.ts
@@ -9,7 +9,7 @@ const serverPORT = process.env.PORT || 8080;
 
 app.use(express.json());
 app.use(cors());
-app.use(express.static(path.join(__dirname, '/..', '/..', 'app', 'build')));
+app.use(express.static(path.join(__dirname, '/..', 'app', 'build')));
 
 app.use('/cards', cardsAPI);
 app.use('/ranking', rankingAPI);
@@ -19,14 +19,17 @@ app.get('/', (req, res) => {
 })
 
 app.all('*', function (req, res) {
-  res.sendFile(path.join(__dirname, '/..', '/..', 'app', 'build', 'index.html'), function (err) {
+  res.sendFile(path.join(__dirname, '/..', 'app', 'build', 'index.html'), function (err) {
       if (err) {
           res.status(500).send(err)
       }
   })
 })
 
-app.listen(process.env.PORT, ()=>{
-  console.log(path.join(__dirname, '/..', '/..', 'app', 'build','index.html'))
+app.listen(serverPORT, ()=>{
+  console.log(path.join(__dirname, 'app', 'build','index.html'))
+  console.log(path.join(__dirname, '/..', 'app', 'build','index.html'))
+  console.log(path.join(__dirname, '/..','/..', 'app', 'build','index.html'))
+  console.log(path.join(__dirname, '/..','/..', '/..','app', 'build','index.html'))
   console.log("***** Card Server has been activated! ******");
 })
