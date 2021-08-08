ed6e95f549046964bb7788f7b13ad42bf3077b5b
Libre255 <briangbv@outlook.com>
2021-08-08 12:27:18 +0200

    11

--- server/src/index.ts
+++ server/src/index.ts
@@ -9,7 +9,7 @@ const serverPORT = process.env.PORT || 8080;
 
 app.use(express.json());
 app.use(cors());
-app.use(express.static(path.join(__dirname + '/../../app/build')));
+app.use('*', express.static(path.join(__dirname, '/..', '/..', 'app', 'build')));
 
 app.use('/cards', cardsAPI);
 app.use('/ranking', rankingAPI);
@@ -19,7 +19,7 @@ app.get('/', (req, res) => {
 })
 
 app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname + '/../../app/build/index.html'), function (err) {
+  res.sendFile(path.join(__dirname, '/..', '/..', 'app', 'build', 'index.html'), function (err) {
       if (err) {
           res.status(500).send(err)
       }
@@ -27,6 +27,6 @@ app.get('/*', function (req, res) {
 })
 
 app.listen(process.env.PORT, ()=>{
-  console.log(path.join(__dirname + '/../../app/build/index.html'))
+  console.log(path.join(__dirname, '/..', '/..', 'app', 'build','index.html'))
   console.log("***** Card Server has been activated! ******");
 })
